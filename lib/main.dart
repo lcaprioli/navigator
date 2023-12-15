877b2ab99f6d331e8e443a319a514cdbcbaeaa76
luizcaprioli <lcaprioli@gmail.com>
2023-12-15 17:30:16 -0300

    Initial Router Experiment STARTED

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+String step1Text = 'Original step 1 text';
 void main() {
   runApp(const MyApp());
 }
@@ -9,60 +10,193 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routerDelegate: MyRouterDelegate(),
+      routeInformationParser: MyRouteInformationParser(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyRouterDelegate extends RouterDelegate<RouteInformation>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouteInformation> {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
 
-  final String title;
+  MyRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
+
+  int _currentStep = 0;
+
+  int get currentStep => _currentStep;
+
+  set currentStep(int value) {
+    if (_currentStep != value) {
+      _currentStep = value;
+      notifyListeners();
+    }
+  }
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        MaterialPage(
+          key: const ValueKey('HomePage'),
+          child: HomePage(goToStep: (step) => currentStep = step),
+        ),
+        if (currentStep > 0)
+          MaterialPage(
+            key: const ValueKey('Step1Page'),
+            child: Step1Page(goToStep: (step) => currentStep = step),
+          ),
+        if (currentStep > 1)
+          MaterialPage(
+            key: const ValueKey('Step2Page'),
+            child: Step2Page(goToStep: (step) => currentStep = step),
+          ),
+        if (currentStep > 2)
+          MaterialPage(
+            key: const ValueKey('Step3Page'),
+            child: Step3Page(goToStep: (step) => currentStep = step),
+          ),
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+        currentStep--;
+        return true;
+      },
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(RouteInformation configuration) async {}
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class MyRouteInformationParser
+    extends RouteInformationParser<RouteInformation> {
+  @override
+  Future<RouteInformation> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    return routeInformation;
   }
+
+  @override
+  RouteInformation restoreRouteInformation(RouteInformation configuration) {
+    return configuration;
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.goToStep});
+  final Function(int) goToStep;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int? _goToStep;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextFormField(
+              onChanged: (value) => setState(() {
+                _goToStep = int.tryParse(value);
+              }),
+            ),
+            ElevatedButton(
+              onPressed:
+                  _goToStep == null ? null : () => widget.goToStep(_goToStep!),
+              child: const Text('Go to Step'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Step1Page extends StatelessWidget {
+  const Step1Page({
+    super.key,
+    required this.goToStep,
+  });
+  final Function(int) goToStep;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Step 1'),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Text(step1Text),
+            ElevatedButton(
+              onPressed: () => goToStep(2),
+              child: const Text('Go to Step 2'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class Step2Page extends StatelessWidget {
+  const Step2Page({
+    super.key,
+    required this.goToStep,
+  });
+  final Function(int) goToStep;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Step 2'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => goToStep(3),
+          child: const Text('Go to Step 3'),
+        ),
+      ),
+    );
+  }
+}
+
+class Step3Page extends StatelessWidget {
+  const Step3Page({super.key, required this.goToStep});
+  final Function(int) goToStep;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Step 3'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => step1Text = 'Modified step 1 text',
+              child: const Text('Modify step 1 text'),
+            ),
+            ElevatedButton(
+              onPressed: () => goToStep(0),
+              child: const Text('Back to Home Page'),
+            ),
+          ],
+        ),
       ),
     );
   }
