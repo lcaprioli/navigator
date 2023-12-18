f874e7179a449b7b66a24599c3b1c4010d383838
luizcaprioli <lcaprioli@gmail.com>
2023-12-18 10:42:34 -0300

    Provider ADDED

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 String step1Text = 'Original step 1 text';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => MyRouterDelegate(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerDelegate: MyRouterDelegate(),
+      routerDelegate: context.read<MyRouterDelegate>(),
       routeInformationParser: MyRouteInformationParser(),
     );
   }
@@ -24,6 +31,13 @@ class MyRouterDelegate extends RouterDelegate<RouteInformation>
 
   MyRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
 
+  List<Page> _pages = [
+    const MaterialPage(
+      key: ValueKey('HomePage'),
+      child: HomePage(),
+    ),
+  ];
+
   int _currentStep = 0;
 
   int get currentStep => _currentStep;
@@ -35,29 +49,51 @@ class MyRouterDelegate extends RouterDelegate<RouteInformation>
     }
   }
 
+  List<Page> _generate(int step) => [
+        const MaterialPage(
+          key: ValueKey('HomePage'),
+          child: HomePage(),
+        ),
+        if (currentStep > 0)
+          const MaterialPage(
+            key: ValueKey('Step1Page'),
+            child: Step1Page(),
+          ),
+        if (currentStep > 1)
+          const MaterialPage(
+            key: ValueKey('Step2Page'),
+            child: Step2Page(),
+          ),
+        if (currentStep > 2)
+          const MaterialPage(
+            key: ValueKey('Step3Page'),
+            child: Step3Page(),
+          ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: navigatorKey,
       pages: [
-        MaterialPage(
-          key: const ValueKey('HomePage'),
-          child: HomePage(goToStep: (step) => currentStep = step),
+        const MaterialPage(
+          key: ValueKey('HomePage'),
+          child: HomePage(),
         ),
         if (currentStep > 0)
-          MaterialPage(
-            key: const ValueKey('Step1Page'),
-            child: Step1Page(goToStep: (step) => currentStep = step),
+          const MaterialPage(
+            key: ValueKey('Step1Page'),
+            child: Step1Page(),
           ),
         if (currentStep > 1)
-          MaterialPage(
-            key: const ValueKey('Step2Page'),
-            child: Step2Page(goToStep: (step) => currentStep = step),
+          const MaterialPage(
+            key: ValueKey('Step2Page'),
+            child: Step2Page(),
           ),
         if (currentStep > 2)
-          MaterialPage(
-            key: const ValueKey('Step3Page'),
-            child: Step3Page(goToStep: (step) => currentStep = step),
+          const MaterialPage(
+            key: ValueKey('Step3Page'),
+            child: Step3Page(),
           ),
       ],
       onPopPage: (route, result) {
@@ -89,8 +125,9 @@ class MyRouteInformationParser
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key, required this.goToStep});
-  final Function(int) goToStep;
+  const HomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -103,15 +140,15 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             ElevatedButton(
-              onPressed: () => goToStep(1),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 1,
               child: const Text('Go to Step 1'),
             ),
             ElevatedButton(
-              onPressed: () => goToStep(2),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 2,
               child: const Text('Go to Step 2'),
             ),
             ElevatedButton(
-              onPressed: () => goToStep(3),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 3,
               child: const Text('Go to Step 3'),
             ),
           ],
@@ -124,9 +161,7 @@ class HomePage extends StatelessWidget {
 class Step1Page extends StatelessWidget {
   const Step1Page({
     super.key,
-    required this.goToStep,
   });
-  final Function(int) goToStep;
 
   @override
   Widget build(BuildContext context) {
@@ -140,7 +175,7 @@ class Step1Page extends StatelessWidget {
           children: [
             Text(step1Text),
             ElevatedButton(
-              onPressed: () => goToStep(2),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 2,
               child: const Text('Go to Step 2'),
             ),
           ],
@@ -153,9 +188,7 @@ class Step1Page extends StatelessWidget {
 class Step2Page extends StatelessWidget {
   const Step2Page({
     super.key,
-    required this.goToStep,
   });
-  final Function(int) goToStep;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -167,7 +200,7 @@ class Step2Page extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             ElevatedButton(
-              onPressed: () => goToStep(3),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 3,
               child: const Text('Go to Step 3'),
             ),
           ],
@@ -178,8 +211,9 @@ class Step2Page extends StatelessWidget {
 }
 
 class Step3Page extends StatelessWidget {
-  const Step3Page({super.key, required this.goToStep});
-  final Function(int) goToStep;
+  const Step3Page({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -196,7 +230,7 @@ class Step3Page extends StatelessWidget {
               child: const Text('Modify step 1 text'),
             ),
             ElevatedButton(
-              onPressed: () => goToStep(0),
+              onPressed: () => context.read<MyRouterDelegate>().currentStep = 0,
               child: const Text('Back to Home Page'),
             ),
           ],
